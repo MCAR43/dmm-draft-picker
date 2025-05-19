270af8bb92f063f989105a6e19e3506203c1e934
Jack Kufa <jack@kufa.io>
2025-05-19 17:21:37 -0400

    literally 3 new tw classes

--- src/lib/components/DraftBoardHeader.svelte
+++ src/lib/components/DraftBoardHeader.svelte
@@ -9,7 +9,7 @@
   </script>
   
   <div class="mb-6">
-    <div class="flex items-center justify-center gap-2 mb-6">
+    <div class="flex md:flex-row flex-col items-center justify-center gap-2 mb-6">
       <h2 class="text-2xl font-bold text-gray-800 text-center drop-shadow-md">{title}</h2>
       <button 
         class="bg-indigo-100 text-indigo-600 border-2 border-indigo-700 rounded px-3 py-1 flex items-center justify-center font-bold hover:bg-indigo-200 transition-colors shadow-sm hover:shadow-md"
@@ -20,7 +20,7 @@
       </button>
     </div>
     
-    <div class="grid grid-cols-6 gap-4">
+    <div class="grid md:grid-cols-6 gap-4 grid-cols-1">
       {#each captains as captain}
         <CaptainHeader captain={captain} />
       {/each}
